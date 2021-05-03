5a6f133b4804831b4a6db8d248e1010fb6d0fbae
spelag <83374186+spelag@users.noreply.github.com>
2021-05-03 11:09:23 +0200

    Add files via upload

--- naloga3.py
+++ naloga3.py
@@ -1,21 +1,39 @@
 n = int(input())
+stalenN = n
+print(n, "=", end=' ')
 
-relevantPrastevila = []
-for i in range(2, n//2):
-    for j in range(2, i):
-        if(i%j==0):
-            break
-    else:
-        relevantPrastevila.append(i)
+potenca = 0
+while n % 2 == 0:
+        n /= 2
+        potenca += 1
+skupaj = 2 ** potenca
+print("2^" + str(potenca), end='*')
+relevantPrastevila = [2]
+while True:
+    i = 1
+    dodanoPraSt = True
+    while dodanoPraSt:
+        i += 1
+        for j in relevantPrastevila:
+            if i%j==0:
+                break
+            if j == relevantPrastevila[-1]:
+                relevantPrastevila.append(i)
+                dodanoPraSt = False
 
-for i in relevantPrastevila:
+    i = relevantPrastevila[-1]
     potenca = 0
+
     while n % i == 0:
         n /= i
         potenca += 1
     if potenca == 1:
         print(i, end='*')
     elif potenca > 1:
-        print(str(i) + str(potenca), sep='^', end='*')
+        print(str(i) + "^" + str(potenca), end='*')
+    skupaj *= i ** potenca
+    if skupaj == stalenN:
+        break
+
 print(end='\b')
 print(" ")
